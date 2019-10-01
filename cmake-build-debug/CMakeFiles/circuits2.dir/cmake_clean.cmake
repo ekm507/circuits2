7798cf5d225568514d7752a7addcc8655b28f452
Erfan Kheyrollahi <ekm507@gmail.com>
2019-10-02 02:43:09 +0330

    clion things

---
+++ cmake-build-debug/CMakeFiles/circuits2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circuits2.dir/main.cpp.obj"
+  "circuits2.exe"
+  "circuits2.exe.manifest"
+  "circuits2.pdb"
+  "libcircuits2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circuits2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
